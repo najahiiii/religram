f094cad74f675aad2c3704aa08e67fa9a1d451c3
Ayra Hikari <AyraHikari@linuxmail.org>
2022-10-17 15:01:54 +0000

    Fixed Callback from help plugin
    Bot trying to filter all incoming but Pyrogram reject it which somehow unsupported incoming filter.
    
    Tested user whitelist and no whitelist and work fine

--- religram/plugins/help.py
+++ religram/plugins/help.py
@@ -30,42 +30,43 @@ async def help_command(client, message):
 
 
 def help_button_callback(flt, _, query):
-	if re.match(r"help_", query.data):
-		return True
+	isAllowed = True
+	if filterWhitelist != filters.incoming:
+		isAllowed = query.from_user.id in filterWhitelist
+	return re.match(r"help_", query.data) and isAllowed
 
 help_button_create = filters.create(help_button_callback)
 
-@app.on_callback_query(filterWhitelist & help_button_create)
+@app.on_callback_query(help_button_create)
 async def help_button(client, query):
 	mod_match = re.match(r"help_module\((.+?)\)", query.data)
 	prev_match = re.match(r"help_prev\((.+?)\)", query.data)
 	next_match = re.match(r"help_next\((.+?)\)", query.data)
 	back_match = re.match(r"help_back", query.data)
-	if True:
-		if mod_match:
-			module = mod_match.group(1)
-			text = "__This is help for the module **{}**__:\n".format(HELP_COMMANDS[module].__MODULE__) \
-				   + HELP_COMMANDS[module].__HELP__
 
-			await query.message.edit(text=text,
+	if mod_match:
+		module = mod_match.group(1)
+		text = "__This is help for the module **{}**__:\n".format(HELP_COMMANDS[module].__MODULE__) \
+			   + HELP_COMMANDS[module].__HELP__
+
+		await query.message.edit(text=text,
 								  reply_markup=InlineKeyboardMarkup(
 										[[InlineKeyboardButton(text="⬅️ Back", callback_data="help_back")]]))
 
-		elif prev_match:
-			curr_page = int(prev_match.group(1))
-			await query.message.edit_text(text=HELP_STRINGS,
-								  reply_markup=InlineKeyboardMarkup(
+	elif prev_match:
+		curr_page = int(prev_match.group(1))
+		await query.message.edit_text(text=HELP_STRINGS,
+									  reply_markup=InlineKeyboardMarkup(
 										paginate_modules(curr_page - 1, HELP_COMMANDS, "help")))
 
-		elif next_match:
-			next_page = int(next_match.group(1))
-			await query.message.edit(text=HELP_STRINGS,
+	elif next_match:
+		next_page = int(next_match.group(1))
+		await query.message.edit(text=HELP_STRINGS,
 								  reply_markup=InlineKeyboardMarkup(
 										paginate_modules(next_page + 1, HELP_COMMANDS, "help")))
 
-		elif back_match:
-			await query.message.edit(text=HELP_STRINGS,
+	elif back_match:
+		await query.message.edit(text=HELP_STRINGS,
 								  reply_markup=InlineKeyboardMarkup(paginate_modules(0, HELP_COMMANDS, "help")))
 
-		return await client.answer_callback_query(query.id)
-
+	return await client.answer_callback_query(query.id)
